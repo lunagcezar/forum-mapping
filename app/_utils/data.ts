06fcb55e49c58150d9022e575fd386b8a33d2947
Luna Gondim Cezar <luna@artemisluna.com.br>
2025-04-10 11:23:15 -0300

    feat: collecting data on front page

--- app/_utils/data.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@/app/_utils/supabase/server"
-
-export async function getSurveys() {
-  const supabase = await createClient()
-
-  const { data } = await supabase.from("surveys").select("*")
-
-  return console.log(data)
-}
